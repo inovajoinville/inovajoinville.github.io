6bc5a0e5b6f5cc921e534f76326fb34e95caeed5
aboutlucas <lucasdiasdemoraes3@gmail.com>
2016-04-27 04:26:52 -0300

    gulp post - minifycss

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
-
+var uncss = require('gulp-uncss');
+var minifycss = require('gulp-minify-css');
 
 gulp.task('build', shell.task(['jekyll build --watch']));
 
@@ -15,3 +16,13 @@ gulp.task('serve', function() {
 });
 
 gulp.task('default',['build', 'serve']);
+
+gulp.task('post', function (){
+	return gulp.src('_site/css/main.css')
+
+	.pipe(uncss({
+		html: ['index.html', 'posts/**/*.html', '_includes/*.html', '_layouts/*.html']
+	}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('_site/css/'))
+});
